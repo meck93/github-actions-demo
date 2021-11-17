3a588b0000fb35f8e89d249dab94a2d637cb14ae
Moritz Eck <moritz.eck@zuehlke.com>
2021-11-17 08:22:46 +0100

    add different workflow types

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # Github Actions Demo
+
+Github has launched Actions in August 2019. 
+
+## Quickstart Guide
+
+[https://docs.github.com/en/actions/quickstart](https://docs.github.com/en/actions/quickstart)
+
+## Publishing Packages
+
+[https://docs.github.com/en/actions/publishing-packages/about-packaging-with-github-actions](https://docs.github.com/en/actions/publishing-packages/about-packaging-with-github-actions)
+
+## Security 
+
+[https://docs.github.com/en/actions/security-guides/security-hardening-for-github-actions](https://docs.github.com/en/actions/security-guides/security-hardening-for-github-actions)
--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,21 @@
 import { serve } from "../dependencies.ts";
+import { add } from "./addition.ts";
 
 const addr = ":8000";
 
 const handler = (request: Request): Response => {
   const url = new URL(request.url);
-  console.log("server.ts 7 url.searchParams:", url.searchParams.get("a"), url.searchParams.get("b"));
-
-  const userAgent = request.headers.get("user-agent") || "Unknown";
-  const responseBody = `Your User-Agent is: ${userAgent}`;
-  return new Response(responseBody, { status: 200 });
+  const a = url.searchParams.get("a");
+  const b = url.searchParams.get("b");
+  if (a !== null && b !== null) {
+    const result = add(Number(a), Number(b));
+    const response = `The result is: ${result}`;
+    return new Response(response, { status: 200 });
+  } else {
+    const userAgent = request.headers.get("user-agent") || "Unknown";
+    const responseBody = `Your User-Agent is: ${userAgent}`;
+    return new Response(responseBody, { status: 200 });
+  }
 };
 
 console.log(`HTTP webserver running at: localhost:8000`);
